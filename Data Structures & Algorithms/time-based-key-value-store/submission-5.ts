fd7fdf6387bbbfad067d69c1973abb21d0e2719a
Thinesh2000 <47693268+Thinesh2000@users.noreply.github.com>
2026-09-02 10:20:16 +0530

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.ts
@@ -0,0 +1,54 @@
+type StampMap = Map<number, string>;
+class TimeMap {
+    keyStore;
+    constructor() {
+        this.keyStore = new Map<string, StampMap>();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key: string, value: string, timestamp: number): void {
+        const timeMap: StampMap = this.keyStore.has(key)
+            ? (this.keyStore.get(key) as StampMap)
+            : (new Map() as StampMap);
+        timeMap.set(timestamp, value);
+        this.keyStore.set(key, timeMap);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key: string, timestamp: number): string {
+        if (!this.keyStore.has(key)) {
+            return "";
+        }
+        const timeMap: StampMap = this.keyStore.get(key) as StampMap;
+        const timeStamps = [...timeMap.keys()];
+        // .sort((a, b) => a - b);
+        if (timeStamps[0] > timestamp) return "";
+        const foundStamp = this.search(timeStamps, timestamp);
+        return timeMap.get(foundStamp)!;
+    }
+
+    search(nums: number[], target: number, l = 0, r = nums.length - 1): number {
+        if (l === r) return nums[l];
+
+        const mid = Math.floor(l + (r - l) / 2);
+
+        if (nums[mid] === target) return nums[mid];
+        else if (nums[mid] > target) return this.search(nums, target, l, mid - 1);
+        else {
+            if ((nums[mid + 1] ?? Infinity) > target) {
+                return nums[mid];
+            }
+
+            return this.search(nums, target, mid + 1, r);
+        }
+    }
+}
